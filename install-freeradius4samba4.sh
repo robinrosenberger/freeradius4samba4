d6ff9efbe697c59bdd1f84bef2f87f33656b20a2
robinrosenberger <robin.rosenberger@gmx.de>
2021-04-12 07:31:06 +0200

    install script for direct install

---
+++ install-freeradius4samba4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update
+apt-get install freeradius -y
+usermod -aG winbindd_priv freerad
+chown root:winbindd_priv /var/lib/samba/winbindd_privileged
+chmod 750 /var/lib/samba/winbindd_privileged
+tar xf configs.tar -C /
+NETBIOS=$(grep workgroup /etc/samba/smb.conf | cut -d '=' -f2 | tr -d ' ')
+sed -i s/LMN/$NETBIOS/g /etc/freeradius/3.0/mods-enabled/mschap
+sed -i s/LMN/$NETBIOS/g /etc/freeradius/3.0/mods-enabled/ntlm_auth
