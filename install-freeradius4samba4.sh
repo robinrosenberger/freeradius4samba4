619c2f21d7b638ddfaef4c1bcb91599a54d0e2fd
robinrosenberger <robin.rosenberger@gmx.de>
2021-04-15 08:13:00 +0200

    absolute path handling, ignore comments in smb.conf

--- install-freeradius4samba4.sh
+++ install-freeradius4samba4.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+workdir=$(dirname $0)
 apt-get update
 apt-get install freeradius -y
 usermod -aG winbindd_priv freerad
 chown root:winbindd_priv /var/lib/samba/winbindd_privileged
 chmod 750 /var/lib/samba/winbindd_privileged
-tar xf configs.tar -C /
-NETBIOS=$(grep workgroup /etc/samba/smb.conf | cut -d '=' -f2 | tr -d ' ')
+tar xf $workdir/configs.tar -C /
+NETBIOS=$(cat /etc/samba/smb.conf | grep -v '#' | cut -d '=' -f2 | tr -d ' ')
 sed -i s/LMN/$NETBIOS/g /etc/freeradius/3.0/mods-enabled/mschap
 sed -i s/LMN/$NETBIOS/g /etc/freeradius/3.0/mods-enabled/ntlm_auth
 sed -i s/mschapv2-and-ntlmv2-only/yes/g /etc/samba/smb.conf
