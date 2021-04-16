f4b63f3564e1a6fc93655bf4849b2dd64d519931
robinrosenberger <robin.rosenberger@gmx.de>
2021-04-16 09:32:03 +0200

    added check for domain membership, improved netbios domain definition

--- install-freeradius4samba4.sh
+++ install-freeradius4samba4.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 workdir=$(dirname $0)
+NETBIOS=$(wbinfo --own-domain)
+[ ! $NETBIOS ] && echo 'Could not determine NETBIOS domain name, is the machine joined to a domain?' exit 1
 apt-get update
 apt-get install freeradius -y
 usermod -aG winbindd_priv freerad
 chown root:winbindd_priv /var/lib/samba/winbindd_privileged
 chmod 750 /var/lib/samba/winbindd_privileged
 tar xf $workdir/configs.tar -C /
-NETBIOS=$(cat /etc/samba/smb.conf | grep workgroup | grep -v '#' | cut -d '=' -f2 | tr -d ' ')
 sed -i s/LMN/$NETBIOS/g /etc/freeradius/3.0/mods-enabled/mschap
 sed -i s/LMN/$NETBIOS/g /etc/freeradius/3.0/mods-enabled/ntlm_auth
 sed -i s/mschapv2-and-ntlmv2-only/yes/g /etc/samba/smb.conf
